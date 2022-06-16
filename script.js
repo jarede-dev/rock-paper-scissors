ca94c34aaab49b9d84890c0621daa10476406357
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-06-16 09:06:41 +0800

    added computerPlay function
    it randomly returns rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+// computerPlay function  will randomly returns rock, paper, or scissors
+function computerPlay(){
+    let getRandom = Math.floor(Math.random() * 10);
+    if(getRandom <= 3){
+        return "Rock";
+    } else if(getRandom <= 6){
+        return "Paper";
+    } else{
+        return "Scissors";
+    }
+}
+
+console.log(computerPlay());
