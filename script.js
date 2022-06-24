06f393f41732890a34530d47ce7a82ec51e69182
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-06-24 12:26:48 +0800

    declared result variable and stored playRound function in there to avoid conflict with console.log

--- script.js
+++ script.js
@@ -18,19 +18,19 @@ function playRound(playerSelection, computerSelection) {
   let getPlayerInsensitive = playerSelection.toLowerCase();
 
   if (getPlayerInsensitive === "rock" && computerSelection === "Rock") {
-    winCounter = winCounter+=0.5;
+    winCounter += 0.5;
     return "It's a tie, both are Rock";
   } else if (getPlayerInsensitive === "rock" && computerSelection === "Paper") {
     winCounter = winCounter;
     return "You Lose! Paper beats Rock";
   } else if (getPlayerInsensitive === "rock" && computerSelection === "Scissors") {
-    winCounter = winCounter++;
+    winCounter += 1;
     return "You win! Rock beats Scissors";
   } else if (getPlayerInsensitive === "paper" && computerSelection === "Rock") {
-    winCounter = winCounter++;
+    winCounter += 1;
     return "You win! Paper beats Rock";
   } else if (getPlayerInsensitive === "paper" && computerSelection === "Paper") {
-    winCounter = winCounter+=0.5;
+    winCounter += 0.5;
     return "It's a tie! Both are Paper";
   } else if (getPlayerInsensitive === "paper" && computerSelection === "Scissors") {
     winCounter = winCounter;
@@ -39,10 +39,10 @@ function playRound(playerSelection, computerSelection) {
     winCounter = winCounter;
     return "You lose! Rock beats Scissors";
   } else if (getPlayerInsensitive === "scissors" && computerSelection === "Scissors") {
-    winCounter = winCounter+=0.5;
+    winCounter += 0.5;
     return "It's a tie! Both are Scissors";
   } else if (getPlayerInsensitive === "scissors" && computerSelection === "Paper") {
-    winCounter = winCounter++;
+    winCounter += 1;
     return "You win! Scissors beat Paper";
   } else {
     return "Check your spelling!";
@@ -52,16 +52,16 @@ function playRound(playerSelection, computerSelection) {
 
 // game function returns the winner after five rounds
 function game() {
-  computerPlay();
   for (let i = 0; i < 5; i++) {
 
     let getSelect = prompt("Choose Rock, Paper, or Scissors", "");
     if (getSelect === null || getSelect === "") {
       alert("You clicked Cancel!");
     }
-    let getWinOrLose = alert(playRound(getSelect, computerPlay()));
+    const result = (playRound(getSelect, computerPlay()))
+    alert(result);
     if (i < 5) {
-      console.log(playRound(getSelect, computerPlay()));
+      console.log(result);
     }
 
     //outputs the winner of 5 games
@@ -69,15 +69,15 @@ function game() {
     if (i === 4) {
       alert("You've played 5 games");
       if (winCounter >= 3) {
-       alert(`You've won ${winCounter} out of 5 games. You win!`);
-       return `You've won ${winCounter} out of 5 games. You win!`;
-    } else if (winCounter === 2.5) {
-       alert(`You've won 2.5 out of 5 games. It's a tie!`);
-       return `You've won 2.5 out of 5 games. It's a tie!`;
-    } else if (winCounter < 2.5) {
-       alert(`You've won ${winCounter} out of 5 games. You lost!`);
-       return `You've won ${winCounter} out of 5 games. You lost!`;
-    }
+        alert(`You've won ${winCounter} out of 5 games. You win!`);
+        return `You've won ${winCounter} out of 5 games. You win!`;
+      } else if (winCounter === 2.5) {
+        alert(`You've won 2.5 out of 5 games. It's a tie!`);
+        return `You've won 2.5 out of 5 games. It's a tie!`;
+      } else if (winCounter < 2.5) {
+        alert(`You've won ${winCounter} out of 5 games. You lost!`);
+        return `You've won ${winCounter} out of 5 games. You lost!`;
+      }
 
     }
   }
